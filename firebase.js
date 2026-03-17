6688ba25485f1fa0895758bf99e41c91468a0373
mallesrikanth013-netizen <mallesrikanth013@gmail.com>
2026-03-17 07:14:32 +0530

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB2NnfhAO2lG3siGJvvRK3FKoy6j__Dvas",
+  authDomain: "g1-student-results.firebaseapp.com",
+  projectId: "g1-student-results",
+  storageBucket: "g1-student-results.firebasestorage.app",
+  messagingSenderId: "46613942170",
+  appId: "1:46613942170:web:3e07427987d4eadbe93423",
+  measurementId: "G-Y11GK4P178"
+};
